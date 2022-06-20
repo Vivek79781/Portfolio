9a56b9cb98ce205bdb4fb15253a5bafad5aac1b2
Vivek79781 <vivekdbz248@gmail.com>
2022-06-20 17:12:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Fragment } from 'react';
+import Header from './components/header/header';
+import Navbar from './components/nav/nav';
+import Home from './components/home/home.js';
+import About from './components/about/about'
+import Projects from './components/projects/projects';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Fragment>
+    <div className='bg-circle1'></div>
+    <div className='bg-circle2'></div>
+    <Header/>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Projects/>
+    <Contact/>
+    <Footer/>
+  </Fragment>
   );
 }
 
